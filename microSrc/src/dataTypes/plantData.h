f89c59222e5ab5937a0c41caa682a109d6317384
aboler <aspenboler@aol.com>
2025-10-24 20:25:16 -0400

    Min. main file + created beginning plant structure

--- microSrc/src/dataTypes/plantData.h
+++ microSrc/src/dataTypes/plantData.h
@@ -1,6 +1,11 @@
+#include <stdint.h>
 
+// Temporary structure for plant data for what could be recorded from sensors
+// May add more/adjust data types as we investigate sensors more
 
-// struct plantData
-// {
-//     uint8_t 
-// };
+struct plantData
+{
+    uint16_t lightData;
+    uint16_t waterData;
+    uint16_t nutrientData;
+};
