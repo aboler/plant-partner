f0b582c4e5c3bdf5ce5c8e6644c1f6b321e8a691
Fernando E. Amado-Pupo <f.amadopupo@ufl.edu>
2025-10-25 13:09:12 -0400

    removed example

--- microSrc/src/main/main.c
+++ microSrc/src/main/main.c
@@ -2,6 +2,17 @@
 
 #include <stdio.h>
 #include "../dataTypes/plantData.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "soc/soc_caps.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
 
 void app_main(void)
 {
@@ -16,7 +27,7 @@ void app_main(void)
     {
         printf("Hello World");
         // Read data in from ADC
-
+        
         // Process data from ADC to get reasonable digital value
 
         // If value below threshold, turn actuator off (Note: actuator is in form of wired LED, so gpio pin down)
