63bd2ca412937e34e46ad29ea368338aab279fb4
aboler <aspenboler@aol.com>
2025-11-06 11:04:59 -0500

    Start of pwm

---
+++ microSrc/src/peripherals/pwm_pump.h
@@ -0,0 +1,11 @@
+#ifndef PWM_PUMP_H
+#define PWM_PUMP_H
+
+#include "driver/mcpwm_prelude.h"
+
+#define PWM_MOTOR   GPIO_NUM_12
+
+// PWM Initialization and Configuration Functions
+
+
+#endif // PWM_PUMP_H
