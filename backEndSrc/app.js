6468ed0379ef8ac78222f316af4e9ddebd8f2190
Nicholas Cimino <ncimino1@ufl.edu>
2025-11-06 11:12:48 -0500

    Nicholas - Adding dependencies for MQTT communication to establish an interface with the ESP32

--- backEndSrc/app.js
+++ backEndSrc/app.js
@@ -3,8 +3,27 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import route from "./routes/sensorRoute.js";
+import cors from "cors";
+import mqtt from "mqtt";
 
+// MQTT Broker Setup
+const MQTT_BROKER_URL = process.env.MQTT_BROKER_URL || 'mqtt://localhost:1883';
+const mqttClient = mqtt.connect(MQTT_BROKER_URL);
+
+mqttClient.on('connect', () => {
+    console.log('Connected to MQTT Broker');
+    mqttClient.subscribe('sensors/updates', (err) => {
+        if (!err) {
+            console.log('Subscribed to sensors/updates topic');
+        } else {
+            console.error('Subscription error:', err);
+        }
+    });
+});
+
+// Backend Server Setup
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 dotenv.config();
