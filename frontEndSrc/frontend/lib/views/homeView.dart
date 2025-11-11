a1b01e38c15a3f153640d6116d7a663c95d9e896
s3luu <sabrinaluu@ufl.edu>
2025-11-11 16:10:14 -0500

    Changed homeView to StatefulWidget to react to user interaction

--- frontEndSrc/frontend/lib/views/homeView.dart
+++ frontEndSrc/frontend/lib/views/homeView.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
 
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,3 +41,40 @@ class HomeView extends StatelessWidget {
     );
   }
 }
+
+// class HomeView extends StatelessWidget {
+//   const HomeView({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Home'),
+//         backgroundColor: Colors.lightGreen,
+//       ),
+//       backgroundColor: const Color.fromARGB(255, 255, 248, 237),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             const Text(
+//               "No Plants Currently",
+//               style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
+//             ),
+//             const SizedBox(height: 24),
+//             ElevatedButton.icon(
+//               onPressed: () {
+//                 Navigator.pushNamed(context, '/addPlant');
+//               },
+//               icon: const Icon(Icons.add),
+//               label: const Text("Add New Plant"),
+//               style: ElevatedButton.styleFrom(
+//                 backgroundColor: Colors.lightGreen,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
