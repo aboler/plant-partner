c323b670e93159fa82a8bc3a14ec08021a933dd6
Nicholas Cimino <ncimino1@ufl.edu>
2025-11-11 13:07:11 -0500

    Nicholas - Adding a new JSON data model for just plants to test for frontend display of data

---
+++ backEndSrc/controller/plantController.js
@@ -0,0 +1,58 @@
+import Plant from "../model/plantModel.js";
+
+export const createPlant = async (req, res) => {
+    try {
+        const plantData = new Plant(req.body);
+        const { plantName } = plantData;
+
+        const plantExist = await Plant.findOne({ plantName });
+        if (plantExist) {
+            return res.status(400).json({ message: 'Plant data already exists' });
+        }
+
+        await plantData.save();
+        res.status(201).json({ message: "Plant created successfully", plantName });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+export const fetchPlants = async (req, res) => {
+    try {
+        const plantData = await Plant.find();
+        res.status(200).json(plantData);
+    } catch (error) {
+        res.status(400).json({ error: 'Failed to fetch plant data' });
+    }
+};
+
+export const getPlantByName = async (req, res) => {
+    try {
+        const name = req.params.plantName;
+        const plantExist = await Plant.find({ plantName: name });
+
+        if (!plantExist) {
+            return res.status(404).json({ message: 'Plant not found' });
+        }
+
+        res.status(200).json(plantExist);
+    } catch (error) {
+        res.status(400).json({ error: 'Failed to fetch plant data' });
+    }
+};
+
+export const updatePlantByName = async (req, res) => {
+    try {
+        const name = req.params.name;
+        const plantExist = await Plant.find({ plantName: name });
+
+        if (!plantExist) {
+            return res.status(404).json({ message: 'Plant not found' });
+        }
+        const updatePlant = await Plant.findByIdAndUpdate(plantExist[0]._id, req.body, { new: true });
+        res.status(200).json(updatePlant);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update plant data' });
+    }
+};
+    
