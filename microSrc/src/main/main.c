f63bbae71574d5028421898446eac99a0d7fc7d8
aboler <aspenboler@aol.com>
2025-11-11 16:28:41 -0500

    Got making version of three inputs connected, am going to configure more for our needs

--- microSrc/src/main/main.c
+++ microSrc/src/main/main.c
@@ -30,63 +30,73 @@ void app_main(void)
     int voltage;
 
     // Initialize ADC Unit 1 Channel 0
-    adc_oneshot_unit_handle_t adc1_handle = adc_oneshot_unit1_init();
+    //adc_oneshot_unit_handle_t adc1_handle = adc_oneshot_unit1_init();
 
     // Configure ADC Channel 0
-    adc_oneshot_channel_config(LIGHT, adc1_handle);
+    //adc_oneshot_channel_config(LIGHT, adc1_handle);
 
     // ADC1 CH0 Calibration Init
-    adc_cali_handle_t adc1_cali_chan0_handle = NULL;
-    bool do_calibration1_chan0 = adc_calibration_init(ADC_UNIT_1, ADC_ATTEN, &adc1_cali_chan0_handle);
+    //adc_cali_handle_t adc1_cali_chan0_handle = NULL;
+    //bool do_calibration1_chan0 = adc_calibration_init(ADC_UNIT_1, ADC_ATTEN, &adc1_cali_chan0_handle);
 
     // Configure LEDs
-    configure_activeHigh_LED(INTERNAL_BLUE_LED_GPIO);
-    configure_activeHigh_LED(EXTERNAL_LED_GPIO);
+    initialize_Interrupts();
+    configure_IO(OUTPUT, INTERNAL_BLUE_LED_GPIO);
+    configure_IO(OUTPUT, EXTERNAL_LED_GPIO);
+    configure_IO(INPUT, SWITCH0_GPIO);
+    configure_IO(INPUT, SWITCH1_GPIO);
+    configure_IO(INPUT, SWITCH2_GPIO);
 
     // Configure PWM and set to 30/256
-    pwm_pump_init();
-    modify_pump_duty_cycle(30);
+    //pwm_pump_init();
+    //modify_pump_duty_cycle(30);
+    uint8_t level = 0;
 
     while (1)
     {
-        // Read raw data in from ADC
-        adc_read(LIGHT, adc1_handle, &adc_raw);
-        ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, ADC_CHANNEL_0, adc_raw);
+        uint8_t temp = gpio_get_level(SWITCH0_GPIO);
+        level = temp;
 
-        // If calibration is enabled, convert raw data to voltage
-        if (do_calibration1_chan0) {
-            adc_rawToVoltage(adc1_cali_chan0_handle, adc_raw, &voltage);
-            ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, ADC_CHANNEL_0, voltage);
-        }
+        ESP_LOGI(TAG, "Level %d", level);
+
+        // // Read raw data in from ADC
+        // adc_read(LIGHT, adc1_handle, &adc_raw);
+        // ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, ADC_CHANNEL_0, adc_raw);
+
+        // // If calibration is enabled, convert raw data to voltage
+        // if (do_calibration1_chan0) {
+        //     adc_rawToVoltage(adc1_cali_chan0_handle, adc_raw, &voltage);
+        //     ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, ADC_CHANNEL_0, voltage);
+        // }
         
-        // Error handling for voltage reading
-        if (voltage < 0) {
-            ESP_LOGW(TAG, "Invalid voltage reading: %d mV", voltage);
-        }
-        // If voltage below threshold, turn LEDs on
-        else if (voltage < LED_THRESHOLD) {
-            // Turn on Internal and External LEDs
-            set_activeHigh_LED(INTERNAL_BLUE_LED_GPIO);
-            set_activeHigh_LED(EXTERNAL_LED_GPIO);
+        // // Error handling for voltage reading
+        // if (voltage < 0) {
+        //     ESP_LOGW(TAG, "Invalid voltage reading: %d mV", voltage);
+        // }
+        // // If voltage below threshold, turn LEDs on
+        // else if (voltage < LED_THRESHOLD) {
+        //     // Turn on Internal and External LEDs
+        //     set_activeHigh_LED(OUTPUT, INTERNAL_BLUE_LED_GPIO);
+        //     set_activeHigh_LED(OUTPUT, EXTERNAL_LED_GPIO);
 
-            // Store data into plant structure
-            plant_data.lightData = voltage;
+        //     // Store data into plant structure
+        //     plant_data.lightData = voltage;
 
-            // Print LED Status and Voltage level as ESP log
-            ESP_LOGI(TAG, "LEDs ON: Voltage %d mV below threshold", voltage);
-        }
-        // If voltage above or equal to threshold, turn LEDs off
-        else if (voltage >= LED_THRESHOLD){
-            // Turn off Internal and External LEDs
-            clear_activeHigh_LED(INTERNAL_BLUE_LED_GPIO);
-            clear_activeHigh_LED(EXTERNAL_LED_GPIO);
+        //     // Print LED Status and Voltage level as ESP log
+        //     ESP_LOGI(TAG, "LEDs ON: Voltage %d mV below threshold", voltage);
+        // }
+        // // If voltage above or equal to threshold, turn LEDs off
+        // else if (voltage >= LED_THRESHOLD){
+        //     // Turn off Internal and External LEDs
+        //     clear_activeHigh_LED(OUTPUT, INTERNAL_BLUE_LED_GPIO);
+        //     clear_activeHigh_LED(OUTPUT, EXTERNAL_LED_GPIO);
 
-            // Store data into plant structure
-            plant_data.lightData = voltage;
+        //     // Store data into plant structure
+        //     plant_data.lightData = voltage;
 
-            // Print statement to confirm LEDs are off
-            ESP_LOGI(TAG, "LEDs OFF: Voltage %d mV above threshold", voltage);
-        }
-        
+        //     // Print statement to confirm LEDs are off
+        //     ESP_LOGI(TAG, "LEDs OFF: Voltage %d mV above threshold", voltage);
+        // }
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
--- microSrc/src/peripherals/gpio_led.h
+++ microSrc/src/peripherals/gpio_led.h
@@ -3,13 +3,20 @@
 
 #include "driver/gpio.h"
 
-#define LED_THRESHOLD 650
-#define EXTERNAL_LED_GPIO GPIO_NUM_14
+#define LED_THRESHOLD          650
+#define EXTERNAL_LED_GPIO      GPIO_NUM_14
 #define INTERNAL_BLUE_LED_GPIO GPIO_NUM_2
+#define SWITCH0_GPIO           GPIO_NUM_21
+#define SWITCH1_GPIO           GPIO_NUM_19
+#define SWITCH2_GPIO           GPIO_NUM_18
+#define OUTPUT                 true
+#define INPUT                  false
 
 // GPIO Configuration & Control for active High LEDs
-void configure_activeHigh_LED(int gpio_num);
-void set_activeHigh_LED(int gpio_num);
-void clear_activeHigh_LED(int gpio_num);
+void configure_IO(bool ioType, uint8_t gpio_num);
+void initialize_Interrupts();
+void set_activeHigh_LED(bool ioType, uint8_t gpio_num);
+void clear_activeHigh_LED(bool ioType, uint8_t gpio_num);
+void toggle_activeHigh_LED(bool ioType, uint8_t gpio_num);
 
 #endif // GPIO_LED_H
