c106ae78929c18558678005470a03645557d7bb3
aboler <aspenboler@aol.com>
2025-11-13 14:13:59 -0500

    Have main sense moisture sensor and report if its dry or not

--- microSrc/src/main/main.c
+++ microSrc/src/main/main.c
@@ -34,8 +34,14 @@ void app_main(void)
     // Initialize ADC for photoresistor
     adc_oneshot_unit_handle_t adc1_handle = adc_oneshot_unit1_init();
     adc_oneshot_channel_config(LIGHT, adc1_handle);
-    adc_cali_handle_t adc1_cali_chan0_handle = NULL;
-    bool chan0_calibration_successful = adc_calibration_init(ADC_UNIT_1, ADC_ATTEN, &adc1_cali_chan0_handle);
+    adc_cali_handle_t light_cali_adc1_handle = NULL;
+    bool light_calibration_successful = adc_calibration_init(ADC_UNIT_1, ADC_ATTEN, &light_cali_adc1_handle);
+
+    // Initialize ADC for moisture sensor
+    //adc_oneshot_unit_handle_t adc1_handle = adc_oneshot_unit1_init();
+    adc_oneshot_channel_config(MOISTURE, adc1_handle);
+    adc_cali_handle_t moisture_cali_adc1_handle = NULL;
+    bool moisture_calibration_successful = adc_calibration_init(ADC_UNIT_1, ADC_ATTEN, &moisture_cali_adc1_handle);
 
     // Configure LEDs
     // initialize_interrupts();
@@ -57,63 +63,80 @@ void app_main(void)
         
         if(currentSwitchLevel != switch0)
         {
-            // Determine if it's dark enough to turn on an LED
+            // Update light value and determine if it's dark enough to turn on an LED
 
             // Read raw data in from ADC
             adc_read(LIGHT, adc1_handle, &adc_raw);
             //ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, ADC_LIGHT_CHANNEL, adc_raw);
 
             // If calibration is enabled, convert raw data to voltage
-            if (chan0_calibration_successful) 
+            if (light_calibration_successful) 
             {
-                adc_rawToVoltage(adc1_cali_chan0_handle, adc_raw, &voltage);
+                adc_rawToVoltage(light_cali_adc1_handle, adc_raw, &voltage);
                 ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, ADC_LIGHT_CHANNEL, voltage);
-            }
-        
-            // Error handling for voltage reading
-            if (voltage < 0) 
-            {
-                ESP_LOGW(TAG, "Invalid voltage reading: %d mV", voltage);
-            }
-            // If voltage below threshold, turn LEDs on
-            else if (voltage < LED_THRESHOLD) 
-            {
-                // Turn on Internal and External LEDs
-                set_activeHigh_LED(OUTPUT, INTERNAL_BLUE_LED_GPIO);
-                set_activeHigh_LED(OUTPUT, EXTERNAL_LED_GPIO);
+            
+                // Error handling for voltage reading
+                if (voltage < 0) 
+                {
+                    ESP_LOGW(TAG, "Invalid voltage reading: %d mV", voltage);
+                }
+                // If voltage below threshold, turn LEDs on
+                else if (voltage < LED_THRESHOLD) 
+                {
+                    // Turn on Internal and External LEDs
+                    set_activeHigh_LED(OUTPUT, INTERNAL_BLUE_LED_GPIO);
+                    set_activeHigh_LED(OUTPUT, EXTERNAL_LED_GPIO);
 
-                // Store data into plant structure
-                plant_data.lightData = voltage;
+                    // Store data into plant structure
+                    plant_data.lightData = voltage;
 
-                // Print LED Status and Voltage level as ESP log
-                ESP_LOGI(TAG, "LEDs ON: Voltage %d mV below threshold", voltage);
-            }
-            // If voltage above or equal to threshold, turn LEDs off
-            else if (voltage >= LED_THRESHOLD)
-            {
-                // Turn off Internal and External LEDs
-                clear_activeHigh_LED(OUTPUT, INTERNAL_BLUE_LED_GPIO);
-                clear_activeHigh_LED(OUTPUT, EXTERNAL_LED_GPIO);
+                    // Print LED Status and Voltage level as ESP log
+                    ESP_LOGI(TAG, "LEDs ON: Voltage %d mV is below threshold", plant_data.lightData);
+                }
+                // If voltage above or equal to threshold, turn LEDs off
+                else if (voltage >= LED_THRESHOLD)
+                {
+                    // Turn off Internal and External LEDs
+                    clear_activeHigh_LED(OUTPUT, INTERNAL_BLUE_LED_GPIO);
+                    clear_activeHigh_LED(OUTPUT, EXTERNAL_LED_GPIO);
 
-                // Store data into plant structure
-                plant_data.lightData = voltage;
+                    // Store data into plant structure
+                    plant_data.lightData = voltage;
 
-                // Print statement to confirm LEDs are off
-                ESP_LOGI(TAG, "LEDs OFF: Voltage %d mV above threshold", voltage);
+                    // Print statement to confirm LEDs are off
+                    ESP_LOGI(TAG, "LEDs OFF: Voltage %d mV is above threshold", plant_data.lightData);
+                }
             }
 
             // Update switch value
             switch0 = currentSwitchLevel;
             vTaskDelay(pdMS_TO_TICKS(200));
         }
-        // currentSwitchLevel = (bool)gpio_get_level(SWITCH1_GPIO);
-        // if(currentSwitchLevel != switch1)
-        // {
-        //     toggle_activeHigh_LED(OUTPUT, INTERNAL_BLUE_LED_GPIO);
-        //     toggle_activeHigh_LED(OUTPUT, EXTERNAL_LED_GPIO);
-        //     switch1 = currentSwitchLevel;
-        //     vTaskDelay(pdMS_TO_TICKS(200));
-        // }
+        currentSwitchLevel = (bool)gpio_get_level(SWITCH1_GPIO);
+        if(currentSwitchLevel != switch1)
+        {
+            // Update moisture value
+
+            // Read raw data in from ADC
+            adc_read(MOISTURE, adc1_handle, &adc_raw);
+
+            // If calibration is enabled, convert raw data to a moisture value
+            if (moisture_calibration_successful) 
+            {
+                voltage = 100 * (2600 - adc_raw)/2600;
+                //adc_rawToVoltage(light_cali_adc1_handle, adc_raw, &voltage);
+                if(voltage < 65)
+                    ESP_LOGI(TAG, "DRY: ADC%d Channel[%d] Showing How Wet: %d percent", ADC_UNIT_1 + 1, ADC_MOISTURE_CHANNEL, voltage);
+                else
+                    ESP_LOGI(TAG, "WET: ADC%d Channel[%d] Showing How Wet: %d percent", ADC_UNIT_1 + 1, ADC_MOISTURE_CHANNEL, voltage);
+            
+                // Update value
+                plant_data.waterData = voltage;
+            }
+            
+            switch1 = currentSwitchLevel;
+            vTaskDelay(pdMS_TO_TICKS(200));
+        }
         // currentSwitchLevel = (bool)gpio_get_level(SWITCH2_GPIO);
         // if(currentSwitchLevel != switch2)
         // {
