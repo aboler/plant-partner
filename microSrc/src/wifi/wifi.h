394c30dd23ee1aa6d65f7343d94bb677f385d919
CarterKreis <cckreis0507@gmail.com>
2025-11-13 12:30:37 -0500

    Wifi build on my laptop now

--- microSrc/src/wifi/wifi.h
+++ microSrc/src/wifi/wifi.h
@@ -21,7 +21,7 @@
 
 
 #define ESP_WIFI_SSID     "Aspen"
-#define ESP_WIFI_PASS      "esp32wifi"
+#define ESP_WIFI_PASS     "esp32wifi"
 #define EXAMPLE_ESP_MAXIMUM_RETRY  4
 #define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_PSK
 #define WIFI_CONNECTED_BIT BIT0
