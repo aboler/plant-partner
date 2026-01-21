b1c8dbc473d4e4a1145e803e80867f1027db0fa6
aboler <aspenboler@aol.com>
2026-01-21 14:05:18 -0500

    Integrated fertilizer motor usage + update pin usage diagram

--- microSrc/src/peripherals/pwm_pump.h
+++ microSrc/src/peripherals/pwm_pump.h
@@ -4,19 +4,23 @@
 #include "driver/gpio.h"
 #include "driver/ledc.h"
 
-#define PWM_MOTOR_PIN         GPIO_NUM_12
-#define PWM_TIMER             LEDC_TIMER_0
-#define PWM_MODE              LEDC_LOW_SPEED_MODE
-#define PWM_CHANNEL           LEDC_CHANNEL_0
-#define PWM_RESOLUTION        LEDC_TIMER_8_BIT
-#define PWM_FREQUENCY_HZ      4000
-#define PWM_DUTY_100_PERCENT  255
-#define PWM_DUTY_50_PERCENT   128
-#define PWM_DUTY_0_PERCENT    0
+#define PWM_WATER_MOTOR_PIN         GPIO_NUM_12
+#define PWM_FERTILIZER_MOTOR_PIN    GPIO_NUM_13
+#define PWM_TIMER                   LEDC_TIMER_0
+#define PWM_MODE                    LEDC_LOW_SPEED_MODE
+#define PWM_WATER_CHANNEL           LEDC_CHANNEL_0
+#define PWM_FERTILIZER_CHANNEL      LEDC_CHANNEL_1
+#define PWM_RESOLUTION              LEDC_TIMER_8_BIT
+#define PWM_FREQUENCY_HZ            4000
+#define PWM_DUTY_100_PERCENT        255
+#define PWM_DUTY_50_PERCENT         128
+#define PWM_DUTY_0_PERCENT          0
+#define FERTLIZER                   true
+#define WATER                       false
 
 
 // PWM Initialization and Configuration Functions
-void pwm_pump_init();
-void modify_pump_duty_cycle(uint8_t duty_cycle);
+void pwm_pump_init(bool component);
+void modify_pump_duty_cycle(bool component, uint8_t duty_cycle);
 
 #endif // PWM_PUMP_H
