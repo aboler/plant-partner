f7e7700f0db0f4d3b3ae30360ecdd618f1ed6dff
Fernando Amado-Pupo <f.amadopupo@ufl.edu>
2026-02-05 03:58:18 -0500

    syntax

--- microSrc/src/communication_protocols/mqtt_proto.h
+++ microSrc/src/communication_protocols/mqtt_proto.h
@@ -2,7 +2,7 @@
 #define MQTT_PROTO_H
 #include "wifi.h"
 #include "mqtt_client.h"
-int mqtt_subscribe(const char *topic, const char *payload, int qos);
+int mqtt_subscribe(const char *topic, int qos);
 int mqtt_publish(const char *topic, const char *payload, int qos);
 bool mqtt_connect(void);
 bool mqtt_poll(char *out, int out_len);
