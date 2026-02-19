031c710337046750cd78f4e13a3c19071d35f842
Nicholas Cimino <ncimino1@ufl.edu>
2026-02-19 12:26:47 -0500

    Nicholas - First test completed for Backend to addresss tasks and send control signals as need

--- backEndSrc/app.js
+++ backEndSrc/app.js
@@ -5,24 +5,74 @@ import bodyParser from "body-parser";
 import route from "./routes/sensorRoute.js";
 import router from "./routes/plantRoute.js";
 import taskRouter from "./routes/taskRoute.js";
+import Task from "./model/taskModel.js";
 import cors from "cors";
 import mqtt from "mqtt";
 import http from "http";
 
 // MQTT Broker Setup
+//const MQTT_BROKER_URL = process.env.MQTT_BROKER_URL || 'mqtt://localhost:1883';
+//const mqttClient = mqtt.connect(MQTT_BROKER_URL);
+
+//Functions for reading control variables 
+async function readControlVar(client) {
+    const taskCursor = Task.find().cursor();
+    for (let t = await taskCursor.next(); t != null; t = await taskCursor.next()) {
+        
+        if(t.status == 'pending') {
+            client.publish('plant_partner/act_tog_en', 'activate', (err) => {
+                if(!err) {
+                    console.log('Successful Actuation Request');
+                } else {
+                    console.log('ERROR: Missed Actuation Request');
+                } 
+            });
+        } 
+        try {
+            await t.deleteOne();
+            console.log('Task deleted');
+        } catch (err) {
+            console.log('ERROR: Task NOT deleted');
+        }
+    }
+}
+
+// MQTT Broker Setup
 const MQTT_BROKER_URL = process.env.MQTT_BROKER_URL || 'mqtt://localhost:1883';
 const mqttClient = mqtt.connect(MQTT_BROKER_URL);
+const SAMPLE_INTERVAL_MS = 5000; // 5 seconds
+const ACT_INTERVAL_MS = 10000; // 10 seconds
 
 mqttClient.on('connect', () => {
     console.log('Connected to MQTT Broker');
-    mqttClient.subscribe('sensors/updates', (err) => {
-        if (!err) {
-            console.log('Subscribed to sensors/updates topic');
-        } else {
-            console.error('Subscription error:', err);
-        }
-    });
+
+
+    setInterval(() => {
+        mqttClient.publish('plant_partner/ack', 'sample', (err) => {
+            if(!err) {
+                console.log('Successful Sampling Request');
+            } else {
+                console.log('ERROR: Missed Sampling Request');
+            }
+        });
+    }, SAMPLE_INTERVAL_MS);
+
+    setInterval(() => {
+        readControlVar(mqttClient);
+    }, ACT_INTERVAL_MS);
+    
 });
+
+//mqttClient.on('connect', () => {
+//    console.log('Connected to MQTT Broker');
+//    mqttClient.subscribe('sensors/updates', (err) => {
+//        if (!err) {
+//            console.log('Subscribed to sensors/updates topic');
+//        } else {
+//            console.error('Subscription error:', err);
+//        }
+//    });
+//});
 
 mqttClient.on('message', (topic, message) => {
     console.log(`Received message on topic ${topic}: ${message.toString()}`);
