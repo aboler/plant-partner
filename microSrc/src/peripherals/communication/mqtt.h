97bb7207bf53a8c282ecd98f43ce986859f1005a
aboler <aspenboler@aol.com>
2026-03-31 12:09:00 -0400

    Merged Fernandos

--- microSrc/src/peripherals/communication/mqtt.h
+++ microSrc/src/peripherals/communication/mqtt.h
@@ -3,11 +3,13 @@
 
 #include <stdbool.h>
 
-#define MQTT_TOPICS "plant_partner/#"
+#define MQTT_TOPIC_ACK "plant_partner/ack"
+#define MQTT_TOPIC_STARTUP "plant_partner/autocare_startup"
+#define MQTT_TOPIC_AUTOCARE "plant_partner/act_tog_en"
 #define MSG_SIZE    256
 #define QOS         0
 #define TOPIC_NAME  64
-#define URI         "mqtt://test.mosquitto.org:1883" // CHANGE IP !!!
+#define URI         "mqtt://172.20.10.9:1883" // CHANGE IP !!!
 
 void mqtt_app_start(void);
 bool mqtt_check_buffer_ready(void);
